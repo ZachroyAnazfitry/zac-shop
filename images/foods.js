f0a95fc9c29a791b90cead4ba31911dd562075e7
ZachAnaz <zachroy04@yahoo.com>
2023-04-11 09:52:05 +0800

    first z-shop commit

---
+++ images/foods.js
@@ -0,0 +1,24 @@
+// select all the icons
+
+const icons =document.querySelectorAll('.section-1-icons i')
+// to fix error after ending of icon
+let i=1
+
+// run functions repeatedly in interval
+setInterval(() =>{
+
+    i++
+    // select class change
+    const icon = document.querySelector('.section-1-icons .change')
+
+    // all classes of the element
+    icon.classList.remove('change')
+
+    // to fix error after ending of icon
+    if (i>icons.length) {
+        icons[0].classList.add('change')
+        i=1
+    } else {
+        icon.nextElementSibling.classList.add('change')
+    }   
+}, 4000)
